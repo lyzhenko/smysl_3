0ffe24ac3aa0760aea18263cdb29c7c7599bf0dc
Александр Лыженко <alex@lyzhenko.ru>
2024-04-23 07:32:04 +0800

    MainTestCase

--- main/tests.py
+++ main/tests.py
@@ -14,3 +14,16 @@ class BasicInstallTestCase(TestCase):
         self.browser.get('http://localhost:8000')
         self.assertTrue('Установка прошла успешно! Поздравляем!' in self.browser.title, 'Тест пройден успешно!')
         self.fail('Not implemented')
+
+
+class MainTestCase(TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_create_url(self):
+        self.browser.get('http://localhost:8000/main')
+        self.assertTrue('http://localhost:8000/main' in self.browser.current_url, 'Тест пройден успешно!')
